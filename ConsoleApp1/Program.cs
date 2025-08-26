c02254b3a4cc633d7c5f4af2b076bd10b0beffef
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2025-08-26 14:14:50 +0200

    i like cookies

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("who are you? ");
+string name = Console.ReadLine();
+Console.WriteLine($"Hej {name}! answer my next question to get a cookie :-)");
+
+Console.WriteLine("are you ok?");
+string answer = Console.ReadLine();
+Console.WriteLine($"good job {name}! here is a digital cookie to improove your mood :p");
+
+Console.WriteLine("what stat would you need to make a cookie?");
+string stat = Console.ReadLine();
+Console.WriteLine($"you now have lvl 100 {stat}");
+
+Console.WriteLine("what did you think of this experiance?");
+Console.ReadLine();
+
+Console.WriteLine(" thank you for your pointless feedback write somathing else to go back.");
+
+
+
+Console.ReadLine();
+
