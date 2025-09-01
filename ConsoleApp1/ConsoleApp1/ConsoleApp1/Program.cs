0257b8e0b82cfccb8788ac954a089eb9aadc2725
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2025-09-01 11:04:21 +0200

    DOOM

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.WebSockets;
+
+bool isAlive = true;
+
+
+Console.WriteLine("Welcome to the ship of DOOM this ship was made to test you and bunch of other people who sadly did not pass");
+Console.WriteLine("click enter to proceed");
+
+Console.ReadLine();
+
+Console.WriteLine("your objektive is to find the correct path of doors and figure out password with the information you are given this information will be found all around the room you are in one a door closes you can never go back if you get stuck you die");
+Console.WriteLine("click enter to proceed");
+
+Console.ReadLine();
+
+
+Console.WriteLine(" you are able to see two rooms where do you go left or right door?");
+
+
+
+string answer = Console.ReadLine();
+
+if (answer == "left")
+{
+    Console.WriteLine("");
+}
+
+if (answer == "right")
+{
+    Console.WriteLine("");
+}
+
+
+
+Console.ReadLine();
+
+
+// ==  are they equal ?
+//isAlive = 6 == 6;
+//isAlive = 6 != 7;
+// != isn´t equal to 
+//{ } kod block doesnt effeck outside
+
+
+//if block doesn´t current it gets skipt
