78a75c569f17b92dea2f347636f82998305c91ad
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2025-09-02 14:55:51 +0200

    monke business

--- ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.WebSockets;
+using System.Security.Cryptography;
 
 bool isAlive = true;
 
@@ -16,18 +17,74 @@ Console.ReadLine();
 
 Console.WriteLine(" you are able to see two rooms where do you go left or right door?");
 
-
+//  killer spaceship, tests and cake 
 
 string answer = Console.ReadLine();
 
 if (answer == "left")
 {
-    Console.WriteLine("");
+    Console.WriteLine("welcome to your first objektive you must lead this simulated team to victory in your way stands an astroid and you cannot avoid it you have in your disposal a ray gun, a bomb and a portal which one would you like to use?  ");
+    string cheese = Console.ReadLine();
+    if (cheese == "bomb")
+    {
+        Console.WriteLine("turns out the bomb was too strong and you exploded restart the code");
+
+        Console.ReadLine();
+    }
+    else if (cheese == "portal")
+    {
+        Console.WriteLine("the team has made it you pass");
+    }
+    else if (cheese == "ray gun")
+    {
+        Console.WriteLine("the hole was too small the astroit destroyed the ship rip");
+    }
+
+
 }
+
+
+
+
 
 if (answer == "right")
 {
-    Console.WriteLine("");
+    Console.WriteLine("you are presented with five objekts a blue cheeseburger, a red gun, a green apple and a yellow shirt. next to that is a paper that says gun, apple, cheeseburger, shirt. and in front of the next door stands some buttons that have the same colors what do you press? and in what order  also you see a cake");
+    string response = Console.ReadLine();
+
+    if (response == "first red then green then blue then yellow")
+    {
+        Console.WriteLine("congrats you win now go have fun in the ship before we get you");
+    }
+    else if (response == "cake")
+    {
+        Console.WriteLine("happy birth day you have the secret ending go fun having the cake now");
+    }
+    else if (response == "grey and white ")
+    {
+        Console.WriteLine(" sorry we do not have color blind mod yet so you have to die");
+    }
+    else if (response == "red,green,blue,yellow")
+    {
+        Console.WriteLine("congrats you win now go have fun in the ship before we get you");
+    }
+    else if (response == "red green blue yellow")
+    {
+        Console.WriteLine("congrats you win now go have fun in the ship before we get you");
+    }
+    else if (response == "red,green,blue and yellow")
+    {
+        Console.WriteLine("congrats you win now go have fun in the ship before we get you");
+    }
+    else if (response == "redgreenblueyellow")
+    {
+        Console.WriteLine("congrats you win now go have fun in the ship before we get you");
+    }
+    else
+    {
+        Console.WriteLine("you die lol");
+    }
+    
 }
 
 
